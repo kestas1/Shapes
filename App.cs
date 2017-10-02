b4dd5fba0c980e485ee347476291a2ab250b376e
kestas1 <exawn1337@gmail.com>
2017-10-02 16:24:28 +0100

    Added properties to Triangle
    Added Accessor and Mutators for Heigth and BaseLength

--- App.cs
+++ App.cs
@@ -13,8 +13,19 @@ namespace Shapes
 
         public void Run()
         {
-            Console.WriteLine("Hello Kestas");
+            Triangle triangle1 = new Triangle();
 
+            Console.WriteLine("Enter the height of the triangle: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the base length of the triangle: ");
+            double baseLength = Convert.ToDouble(Console.ReadLine());
+            
+            triangle1.Height = height;
+            triangle1.BaseLength = baseLength;
+
+            Console.Write("The area of the triangle is ");
+            Console.WriteLine( triangle1.GetArea() );
         }
     
     }
